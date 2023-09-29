33c6095b7ffacd48f8c9b8c558dac4b6db0f2020
seo-kh <gospelseokh@gmail.com>
2023-09-29 16:56:50 +0900

    Initial Commit

---
+++ Plot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plot
+//
+//  Created by james seo on 9/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Plot/PlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlotApp.swift
+//  Plot
+//
+//  Created by james seo on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct PlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
